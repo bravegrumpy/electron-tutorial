1a3b4daa0c71e18636bfe650438e9ea45b759f2b
Robin Zollner <code@bravegrumpy.com>
2024-10-07 21:15:28 -0400

    trying to fix promise bug

--- preload.js
+++ preload.js
@@ -5,4 +5,4 @@ contextBridge.exposeInMainWorld('versions', {
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron,
     ping: () => ipcRenderer.invoke(`ping`)
-})
+});
