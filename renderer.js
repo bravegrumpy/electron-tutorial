250c57e676eccaf797d5e188daf1b1ec016cd2e8
Robin Zollner <code@bravegrumpy.com>
2024-10-07 23:42:00 -0400

    adding in react installation

--- renderer.js
+++ renderer.js
@@ -18,3 +18,10 @@ document.getElementById('reset-to-system').addEventListener('click', async () =>
     await window.darkMode.system()
     document.getElementById('theme-source').innerHTML = 'System'
 })
+
+import { createRoot } from 'react-dom/client';
+
+document.body.getElementsById("react-app").innerHTML = `div id="app"></div>`;
+
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello from React</h1>);
